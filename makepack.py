4a9c15f837edcba8673691da76fb61deef475f72
www10177 <www10177@gmail.com>
2019-12-30 21:08:02 +0800

    fix confirm button, add sleep time to prevent click miss

--- makepack.py
+++ makepack.py
@@ -18,6 +18,7 @@ def load_driver():
 
 
 def makepack(list_fn='./appid.txt'):
+	sleep_time= 0.5
     driver = load_driver()
     if not os.path.isfile(list_fn):
         print ('%s is not existed'%list_fn)
@@ -41,12 +42,12 @@ def makepack(list_fn='./appid.txt'):
             print (appid, " : ", select.first_selected_option.text, ' is not available to make pack, may because of time limit\n')
             if not skip_flag:
                 skip_text = input('Press Enter to Continue\nOr key in "y" in console to SKIP all confirmation\nor just alt+f4 to quit\n')
-                if skip_text.strip().lower() == 's':
+                if skip_text.strip().lower() == 'y':
                     skip_flag = True
             continue
         if not skip_flag:
             skip_text = input('Press Enter to make pack \nOr key in "y" in console to SKIP all confirmation\nor just alt+f4 to quit\n')
-            if skip_text.strip().lower() == 's':
+            if skip_text.strip().lower() == 'y':
                 skip_flag = True
         radio_name = 'booster_tradability_preference_%s_%s'
         #Click Nottradable first 
@@ -54,8 +55,11 @@ def makepack(list_fn='./appid.txt'):
         #If tradable make tradable card pack
         driver.find_element_by_id(radio_name%('tradable',appid)).click()
         driver.find_element_by_class_name("btn_makepack").click()
+        sleep(sleep_time)
         driver.find_element_by_class_name("newmodal_buttons").find_element_by_class_name("btn_green_white_innerfade").click()
+        sleep(sleep_time)
         driver.find_element_by_class_name("newmodal_close").click()
+        sleep(sleep_time)
         print('='*50)
 
     input('Make sure every actions is done! \nThen Press enter to quit')
